81d48a4496f613741a90eed8b1e410d5a438e427
Emanuele Garolla <e.garolla@reply.it>
2018-12-01 10:18:08 +0100

    Starting a new proj

---
+++ LabCamp/LabCamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LabCamp
+//
+//  Created by Emanuele Garolla on 01/12/2018.
+//  Copyright © 2018 Emanuele Garolla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
